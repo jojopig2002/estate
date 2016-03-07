bab6bd378b2894b532b7b4876f2c2dc263648699
jojopig2002 <jojopig2002@hotmail.com>
2016-03-07 20:51:28 +1100

    scaffolding

---
+++ Gruntfile.js
@@ -0,0 +1,130 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['js/*.js'],
+        dest: 'build/js/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'build/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    qunit: {
+      all: {
+        options: {
+          urls: [
+            'http://localhost/housemap/test/test.html'
+          ]
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: '.'
+        }
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'js/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        force: true,
+        globals: {
+          jQuery: true,
+          $: true,
+          alert: true,
+          window: true,
+          stop: true,
+          angular: true,
+          google: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint'],
+        options: {
+          livereload: true
+        }
+      },
+      css: {
+        files: ['css/*.less'],
+        options: {
+          livereload: 1337
+        }
+      }
+    },
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
+    },
+    protractor: {
+      options: {
+        configFile: "node_modules/protractor/example/conf.js", // Default config file 
+        keepAlive: true, // If false, the grunt process stops when the test fails. 
+        noColor: false, // If true, protractor will not use colors in its output. 
+        args: {
+          // Arguments passed to the command 
+        }
+      },
+      your_target: { // Grunt requires at least one target to run so you can simply put 'all: {}' here too. 
+        options: {
+          configFile: "test/e2e/conf.js", // Target-specific config file 
+          args: {} // Target-specific arguments 
+        }
+      },
+    },
+    protractor_webdriver: {
+      options: {
+        // Task-specific options go here. 
+        command: 'webdriver-manager start',
+        path: ' /usr/local/lib/node_modules/protractor/selenium/chromedriver',
+        keepAlive: true
+      },
+      your_target: {
+        options: {
+          // path: '',
+          // command: 'webdriver-manager start'
+        }
+      }
+    }
+  });
+  
+  // grunt.loadNpmTasks
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-protractor-webdriver');
+  grunt.loadNpmTasks('grunt-protractor-runner');
+
+  grunt.registerTask('lint', ['jshint']);
+  grunt.registerTask('unittest', ['karma']);
+  grunt.registerTask('minify', ['concat', 'uglify']);
+  grunt.registerTask('e2etest', ['protractor']);
+
+  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+
+};
